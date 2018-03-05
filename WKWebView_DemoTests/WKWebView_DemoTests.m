ac7c113bfc3250146487120a599e5bf17f242bb0
admin <admin@bubu100.local>
2018-03-05 13:07:05 +0800

    Initial Commit

---
+++ WKWebView_DemoTests/WKWebView_DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  WKWebView_DemoTests.m
+//  WKWebView_DemoTests
+//
+//  Created by admin on 2018/3/5.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WKWebView_DemoTests : XCTestCase
+
+@end
+
+@implementation WKWebView_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
